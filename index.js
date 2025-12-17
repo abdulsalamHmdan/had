2b31af29961a523ee983829e4f8b2ae24dd8fff5
abdulsalam hmdan <abdulsalam.hmdan@gmail.com>
2025-12-17 18:25:25 +0300

    aaaaa

--- index.js
+++ index.js
@@ -124,7 +124,8 @@ app.post('/delete', async function (req, res) {
 
 
 app.post('/login', express.urlencoded({ extended: false }), function (req, res) {
-    if (req.body.user == "1014772139" && req.body.pass == "0505152317") {
+    console.log(req.body);
+    if (req.body.user == "1014772139" && req.body.pass == +"0505152317") {
         req.session.regenerate(function (err) {
             if (err) next(err)
             req.session.admin = true
